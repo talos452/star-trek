dde39878f8ce61eb12a16ffb0faf0c55ac636738
VulcanForge <vulcanforge@gmail.com>
2016-08-14 09:16:47 -0300

    Began implementing ChunkProviderSpace
    Set up failsafes and renamed ambiguous parameter names.

--- main/java/startrekmod/generation/dimension/space/ChunkProviderSpace.java
+++ main/java/startrekmod/generation/dimension/space/ChunkProviderSpace.java
@@ -5,105 +5,80 @@ import net.minecraft.util.IProgressUpdate;
 import net.minecraft.world.*;
 import net.minecraft.world.chunk.*;
 
-import java.util.List;
+import java.util.*;
 
 public class ChunkProviderSpace implements IChunkProvider
 {
-	public ChunkProviderSpace()
+	public ChunkProviderSpace() {}
+
+	@Override
+	public boolean chunkExists(int x, int z)
 	{
-		// TODO Auto-generated constructor stub
+		return true;
 	}
 
 	@Override
-	public boolean chunkExists(int p_73149_1_, int p_73149_2_)
+	public Chunk provideChunk(int x, int z)
 	{
-		// TODO Auto-generated method stub
-		return false;
+		return loadChunk(x, z);
 	}
 
 	@Override
-	public Chunk provideChunk(int p_73154_1_, int p_73154_2_)
+	public Chunk loadChunk(int x, int z)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public Chunk loadChunk(int p_73158_1_, int p_73158_2_)
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public void populate(IChunkProvider provider, int x, int z) {}
 
 	@Override
-	public void populate(IChunkProvider p_73153_1_, int p_73153_2_, int p_73153_3_)
+	public boolean saveChunks(boolean saveAllChunks, IProgressUpdate progress)
 	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean saveChunks(boolean p_73151_1_, IProgressUpdate p_73151_2_)
-	{
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean unloadQueuedChunks()
 	{
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canSave()
 	{
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public String makeString()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return "SpaceLevelSource";
 	}
 
 	@Override
-	public List getPossibleCreatures(EnumCreatureType p_73155_1_, int p_73155_2_, int p_73155_3_, int p_73155_4_)
+	public List getPossibleCreatures(EnumCreatureType filter, int x, int y, int z)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return new ArrayList(0);
 	}
 
+	//this method may have something to do with fixed-location structures
 	@Override
-	public ChunkPosition func_147416_a(World p_147416_1_, String p_147416_2_, int p_147416_3_, int p_147416_4_,
-			int p_147416_5_)
+	public ChunkPosition func_147416_a(World world, String structureID, int x, int y, int z)
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public int getLoadedChunkCount()
 	{
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public void recreateStructures(int p_82695_1_, int p_82695_2_)
-	{
-		// TODO Auto-generated method stub
-
-	}
+	public void recreateStructures(int x, int z) {}
 
 	@Override
-	public void saveExtraData()
-	{
-		// TODO Auto-generated method stub
-
-	}
+	public void saveExtraData() {}
 
 }
